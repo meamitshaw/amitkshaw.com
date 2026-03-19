3fed17b6c117c9d59b9d5666c92c30de606fea57
meamitshaw <154111028+meamitshaw@users.noreply.github.com>
2026-03-19 15:06:42 -0400

    Update
    Basic update in interests section and add ed train photo.

---
+++ interests.js
@@ -0,0 +1,48 @@
+const interests = [
+  {
+    type: "railroad",
+    title: "Model Railroading (HO Scale)",
+    description: "Captured by the mechanical precision of brass and diecast locomotives...",
+    cover: "assets/train-collection-main.jpg",
+    images: [
+      "assets/loco-large.jpg",
+      "assets/freight-car.jpg"
+    ]
+  },
+
+  {
+    type: "ai",
+    title: "AI-Driven Transformation",
+    description: "Exploring Gen-AI and ML in financial ecosystems..."
+  },
+
+  {
+    type: "travel",
+    title: "Exploring the World",
+    description: "Travel shapes global architectural thinking...",
+    places: [
+      {
+        name: "Tokyo & Kyoto, Japan",
+        cover: "assets/japan-kyoto.jpg",
+        images: [
+          "assets/japan-kyoto.jpg",
+          "assets/japan1.jpg"
+        ]
+      },
+      {
+        name: "Alaska, USA",
+        cover: "assets/alaska.jpg",
+        images: [
+          "assets/alaska.jpg"
+        ]
+      },
+      {
+        name: "Colorado, USA",
+        cover: "assets/colorado.jpg",
+        images: [
+          "assets/colorado.jpg"
+        ]
+      }
+    ]
+  }
+];
